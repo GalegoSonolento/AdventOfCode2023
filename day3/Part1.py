243f5faae0d8d74f320f85383f4b492bdb8ee1a8
HaagHenrique <henriquehaag11@gmail.com>
2023-12-04 20:13:11 -0300

    day 3

--- day3/Part1.py
+++ day3/Part1.py
@@ -30,8 +30,9 @@ sumTot = 0
 numberPlacement = 0
 # Precisa criar algum tipo de regra pra ir tirando os números daqui, um contador até 3 pra tirar o último, ir retirando
 # conforme passa, contar até 2, sla
-previousNum = []
+previousNum = 1000000000000000
 currentNum = 0
+counterToKillPreviousNum = 0
 for i in inputDict:
     for j in range(0, len(inputDict[i])):
         if not inputDict[i][j].isnumeric() and not inputDict[i][j] == ".":
@@ -39,56 +40,72 @@ for i in inputDict:
                 if j - 1 >= 0:
                     if inputDict[i-1][j-1].isnumeric():
                         currentNum = findInteger(i - 1, j - 1)
-                        if currentNum not in previousNum:
-                            previousNum.append(currentNum)
+                        if currentNum != previousNum:
+                            previousNum = currentNum
                             sumTot += currentNum
+                    else:
+                        currentNum = 1000000000000000
                 if j >= 0:
                     if inputDict[i-1][j].isnumeric():
                         currentNum = findInteger(i - 1, j)
-                        if currentNum not in previousNum:
-                            previousNum.append(currentNum)
+                        if currentNum != previousNum:
+                            previousNum = currentNum
                             sumTot += currentNum
+                    else:
+                        currentNum = 1000000000000000
                 if j + 1 < len(inputDict[i-1]):
                     if inputDict[i-1][j+1].isnumeric():
                         currentNum = findInteger(i - 1, j + 1)
-                        if currentNum not in previousNum:
-                            previousNum.append(currentNum)
+                        if currentNum != previousNum:
+                            previousNum = currentNum
                             sumTot += currentNum
+                    else:
+                        currentNum = 1000000000000000
             if i >= 0:
                 if j - 1 >= 0:
                     if inputDict[i][j-1].isnumeric():
                         currentNum = findInteger(i, j - 1)
-                        if currentNum not in previousNum:
-                            previousNum.append(currentNum)
+                        if currentNum != previousNum:
+                            previousNum = currentNum
                             sumTot += currentNum
+                    else:
+                        currentNum = 1000000000000000
                 # if j >= 0:
                 #     if inputDict[i][j].isnumeric():
                 #         sumTot += findInteger(i, j)
                 if j + 1 < len(inputDict[i]):
                     if inputDict[i][j+1].isnumeric():
                         currentNum = findInteger(i, j + 1)
-                        if currentNum not in previousNum:
-                            previousNum.append(currentNum)
+                        if currentNum != previousNum:
+                            previousNum = currentNum
                             sumTot += currentNum
+                    else:
+                        currentNum = 1000000000000000
             if i + 1 < len(inputDict):
                 if j - 1 >= 0:
                     if inputDict[i+1][j-1].isnumeric():
                         currentNum = findInteger(i + 1, j - 1)
-                        if currentNum not in previousNum:
-                            previousNum.append(currentNum)
+                        if currentNum != previousNum:
+                            previousNum = currentNum
                             sumTot += currentNum
+                    else:
+                        currentNum = 1000000000000000
                 if j > 0 and j < len(inputDict[i+1]):
                     if inputDict[i+1][j].isnumeric():
                         currentNum = findInteger(i + 1, j)
-                        if currentNum not in previousNum:
-                            previousNum.append(currentNum)
+                        if currentNum != previousNum:
+                            previousNum = currentNum
                             sumTot += currentNum
+                    else:
+                        currentNum = 1000000000000000
                 if j+1 < len(inputDict[i+1]):
                     if inputDict[i+1][j+1].isnumeric():
                         currentNum = findInteger(i + 1, j + 1)
-                        if currentNum not in previousNum:
-                            previousNum.append(currentNum)
+                        if currentNum != previousNum:
+                            previousNum = currentNum
                             sumTot += currentNum
+                    else:
+                        currentNum = 1000000000000000
 
 
 
